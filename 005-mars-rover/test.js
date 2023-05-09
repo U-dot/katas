b84f544b8e34cb2cd21a777232bdeff7d7ab1cfd
soleil <mbotello@unal.edu.co>
2023-05-08 20:37:25 -0500

    set up for solving the kata on js

---
+++ 005-mars-rover/test.js
@@ -0,0 +1,20 @@
+import { expect } from "chai";
+import { executeInstructions } from "./code";
+
+describe("", () => {
+    describe("", () => {
+        it("", () => {
+            jest.spyOn(console,"log")
+            executeInstructions({
+                x: 0,
+                y: 0,
+                direction: 'N',
+                plateau: {
+                    width: 0,
+                    height: 0,
+                }
+            })
+
+        });
+    });
+});
