e668ed476d897b05d51f4b4c839df12c4f3662c7
soleil <mbotello@unal.edu.co>
2023-05-08 21:17:45 -0500

    rover accepts dirrections

--- 005-mars-rover/code.js
+++ 005-mars-rover/code.js
@@ -1,1 +1,38 @@
-export function executeInstructions(){}
+export function executeInstructions(rover){
+    console.log(`0 0 ${newDirection(rover)}\n`);
+}
+
+function newDirection(rover){
+    if (rover.instructions === 'R'){
+        return turnRight(rover)
+    }
+    else {
+        return turnLeft(rover)
+    }
+}
+
+function turnLeft(rover){
+    if(rover.direction==='W'){
+        return 'S';
+    }
+    if(rover.direction==='E'){
+        return 'N';
+    }
+    if(rover.direction==='S'){
+        return 'E';
+    }
+    return 'W';
+}
+
+function turnRight(rover){
+    if(rover.direction==='W'){
+        return 'N';
+    }
+    if(rover.direction==='E'){
+        return 'S';
+    }
+    if(rover.direction==='S'){
+        return 'W';
+    }
+    return 'E';
+}
--- 005-mars-rover/test.js
+++ 005-mars-rover/test.js
@@ -1,20 +1,156 @@
-import { expect } from "chai";
 import { executeInstructions } from "./code";
 
-describe("", () => {
-    describe("", () => {
-        it("", () => {
-            jest.spyOn(console,"log")
-            executeInstructions({
-                x: 0,
-                y: 0,
-                direction: 'N',
-                plateau: {
-                    width: 0,
-                    height: 0,
-                }
-            })
-
+describe("Given a sole rover in", () => {
+    describe("0,0 facing north", () => {
+        describe("When instructed to turn left", () => {
+            it("It faces west", () => {
+                jest.spyOn(console,"log")
+                executeInstructions({
+                    x: 0,
+                    y: 0,
+                    direction: 'N',
+                    instructions: 'L',
+                    plateau: {
+                        width: 0,
+                        height: 0,
+                    }
+                });
+                expect(console.log).toBeCalledWith("0 0 W\n")
+                expect(console.log).toBeCalledTimes(1)
+                console.log.mockReset();
+            });
+        });
+        describe("When instructed to turn right", () => {
+            it("It faces east", () => {
+                jest.spyOn(console,"log")
+                executeInstructions({
+                    x: 0,
+                    y: 0,
+                    direction: 'N',
+                    instructions: 'R',
+                    plateau: {
+                        width: 0,
+                        height: 0,
+                    }
+                });
+                expect(console.log).toBeCalledWith("0 0 E\n")
+                expect(console.log).toBeCalledTimes(1)
+                console.log.mockReset();
+            });
+        });
+    });
+    describe("0,0 facing west", () => {
+        describe("When instructed to turn left", () => {
+            it("It faces south", () => {
+                jest.spyOn(console,"log")
+                executeInstructions({
+                    x: 0,
+                    y: 0,
+                    direction: 'W',
+                    instructions: 'L',
+                    plateau: {
+                        width: 0,
+                        height: 0,
+                    }
+                });
+                expect(console.log).toBeCalledWith("0 0 S\n")
+                expect(console.log).toBeCalledTimes(1)
+                console.log.mockReset();
+            });
+        });
+        describe("When instructed to turn right", () => {
+            it("It faces south", () => {
+                jest.spyOn(console,"log")
+                executeInstructions({
+                    x: 0,
+                    y: 0,
+                    direction: 'W',
+                    instructions: 'R',
+                    plateau: {
+                        width: 0,
+                        height: 0,
+                    }
+                });
+                expect(console.log).toBeCalledWith("0 0 N\n")
+                expect(console.log).toBeCalledTimes(1)
+                console.log.mockReset();
+            });
+        });
+    });
+    describe("0,0 facing east", () => {
+        describe("When instructed to turn left", () => {
+            it("It faces north", () => {
+                jest.spyOn(console,"log")
+                executeInstructions({
+                    x: 0,
+                    y: 0,
+                    direction: 'E',
+                    instructions: 'L',
+                    plateau: {
+                        width: 0,
+                        height: 0,
+                    }
+                });
+                expect(console.log).toBeCalledWith("0 0 N\n")
+                expect(console.log).toBeCalledTimes(1)
+                console.log.mockReset();
+            });
+        });
+        describe("When instructed to turn right", () => {
+            it("It faces south", () => {
+                jest.spyOn(console,"log")
+                executeInstructions({
+                    x: 0,
+                    y: 0,
+                    direction: 'E',
+                    instructions: 'R',
+                    plateau: {
+                        width: 0,
+                        height: 0,
+                    }
+                });
+                expect(console.log).toBeCalledWith("0 0 S\n")
+                expect(console.log).toBeCalledTimes(1)
+                console.log.mockReset();
+            });
+        });
+    });
+    describe("0,0 facing south", () => {
+        describe("When instructed to turn left", () => {
+            it("It faces east", () => {
+                jest.spyOn(console,"log")
+                executeInstructions({
+                    x: 0,
+                    y: 0,
+                    direction: 'S',
+                    instructions: 'L',
+                    plateau: {
+                        width: 0,
+                        height: 0,
+                    }
+                });
+                expect(console.log).toBeCalledWith("0 0 E\n")
+                expect(console.log).toBeCalledTimes(1)
+                console.log.mockReset();
+            });
+        });
+        describe("When instructed to turn right", () => {
+            it("It faces west", () => {
+                jest.spyOn(console,"log")
+                executeInstructions({
+                    x: 0,
+                    y: 0,
+                    direction: 'S',
+                    instructions: 'R',
+                    plateau: {
+                        width: 0,
+                        height: 0,
+                    }
+                });
+                expect(console.log).toBeCalledWith("0 0 W\n")
+                expect(console.log).toBeCalledTimes(1)
+                console.log.mockReset();
+            });
         });
     });
 });
